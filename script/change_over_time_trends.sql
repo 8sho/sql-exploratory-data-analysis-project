bdfe8f30d983c095cfb088788c2e701a46077cf0
Shobhit Mudgal <137764716+8sho@users.noreply.github.com>
2026-09-20 19:57:28 +0530

    Add SQL queries for sales performance analysis
    Added SQL queries to analyze sales performance over time, including total sales and customer counts by year and month.

--- script/change_over_time_trends.sql
+++ script/change_over_time_trends.sql
@@ -1,1 +1,30 @@
 
+-- analyze sales performance over time 
+   SELECT 
+   YEAR (order_date) as order_year,
+   SUM (sales) as total_sales
+   from GOLD.facts_sales
+   where YEAR (order_date) IS NOT NULL
+   GROUP BY YEAR (order_date)
+   order by YEAR (order_date)
+
+-- analyze sales performance along with customers over time 
+   SELECT 
+   YEAR (order_date) as order_year,
+   SUM (sales) as total_sales,
+   COUNT (DISTINCT customer_key) as total_customers
+   from GOLD.facts_sales
+   where YEAR (order_date) IS NOT NULL
+   GROUP BY YEAR (order_date)
+   order by YEAR (order_date)
+-- looking at the results we get high-level overview insights that helps with startegic decision making 
+-- we can use datetrunc function to get months along with year in the same column
+
+   SELECT 
+   DATETRUNC (month , order_date) as order_year,
+   SUM (sales) as total_sales,
+   COUNT (DISTINCT customer_key) as total_customers
+   from GOLD.facts_sales
+   where YEAR (order_date) IS NOT NULL
+   GROUP BY DATETRUNC (month , order_date)
+   order by DATETRUNC (month , order_date)
