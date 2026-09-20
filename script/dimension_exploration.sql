a039202118d63fe47ec9a2d132a2eb3d50b1c89d
Shobhit Mudgal <137764716+8sho@users.noreply.github.com>
2026-09-20 19:51:21 +0530

    Add queries for customer countries and product categories
    Added SQL queries to explore countries and product categories.

--- script/dimension_exploration.sql
+++ script/dimension_exploration.sql
@@ -1,1 +1,9 @@
 
+-- explore all the countries our customers come from 
+
+select DISTINCT country from gold.dim_customers
+
+-- explore all categories the major division
+
+select DISTINCT product_cat , product_sub_cat , product_name from gold.dim_products
+order by 1,2,3
