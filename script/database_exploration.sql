3cfdb0e7c39a11e0cde12f8b8220bc69af04ab24
Shobhit Mudgal <137764716+8sho@users.noreply.github.com>
2026-09-20 19:50:16 +0530

    Update database_exploration.sql

--- script/database_exploration.sql
+++ script/database_exploration.sql
@@ -1,1 +1,12 @@
+--1. Database Exploration
+
+-- explore all objects in the database 
+
+SELECT *
+from INFORMATION_SCHEMA.TABLES
+
+-- explore all columns in the database 
+
+SELECT*
+from INFORMATION_SCHEMA.COLUMNS
 
